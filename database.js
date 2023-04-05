6d7d5b8a16df621657dae4780cfa62a294b44c53
GoblinStorm <98617466+GoblinStorm@users.noreply.github.com>
2023-04-04 20:52:45 -0400

    Add files via upload
    My added code for the bathroom buttons

---
+++ database.js
@@ -0,0 +1,96 @@
+import mysql from "mysql2";
+import dotenv from "dotenv";
+dotenv.config();
+
+// Pool connects to the MySQL database using environment variables
+// You should create a .env file in the same folder as this file to assign each environment variable
+// .env file should just be four lines for each variable like below example
+// process.env.MYSQL_HOST="localhost"
+// process.env.MYSQL_DATABAE="realestate"
+const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
+}).promise();
+
+
+
+// Below are the functions for querying data
+// These functions get exported to server.js and the server sends data to the HTML webpage
+// NOTE: the pool.query method returns an array of TWO arrays. The first array is the query results, the second array is just metadata.
+// So make sure to grab only the 0th element of pool.query
+
+// Select queries will always join all columns from home with owner.name and sale.price as this is the information we want to display on the page
+// Selects all from home joined with owner.name and sale.price
+async function getHomes() {
+	const rows = (await pool.query("select home.*, owner.name, sale.price from home left join owner on home.owner_ssn = owner.ssn left join sale on sale.home_id = home.home_id;"))[0];
+	return rows;
+}
+
+// Inserts a new home with default values (all null)
+async function insert() {
+	await pool.query("insert into home values ();");
+}
+
+// Updates all rows
+async function update(id, type, sqft, floors, bedrooms, bathrooms, landSize, year) {
+	for (let i = 0; i < type.length; i ++) {
+		// If any values are an empty string, assign them as null so rows arent updated with empty strings
+		if (type[i] == "") { type[i] = null; } else { type[i] = "'"+type[i]+"'"; }
+		if (sqft[i] == "") { sqft[i] = null; }
+		if (floors[i] == "") { floors[i] = null; }
+		if (bedrooms[i] == "") { bedrooms[i] = null; }
+		if (bathrooms[i] == "") { bathrooms[i] = null; }
+		if (landSize[i] == "") { landSize[i] = null; }
+		if (year[i] == "") { year[i] = null; }
+		await pool.query("update home set type="+type[i]+", sqft="+sqft[i]+", floors="+floors[i]+", bedrooms="+bedrooms[i]+", bathrooms="+bathrooms[i]+", land_size="+landSize[i]+", year="+year[i]+" where home_id="+id[i]+";");
+	}
+}
+
+// Deletes a row
+// Also sets home_id = null on any child sales
+async function deleteHome(id) {
+	await pool.query("set foreign_key_checks = 0;");
+	await pool.query("delete from home where home_id = " + id + ";");
+	await pool.query("delete from sale where home_id = " + id + ";");
+	await pool.query("set foreign_key_checks = 1;");
+}
+
+// Selects and orders rows by owner.name ascending
+async function getHomesOwnerAsc() {
+	const rows = (await pool.query("select home.*, owner.name, sale.price from home left join owner on home.owner_ssn = owner.ssn left join sale on sale.home_id = home.home_id order by owner.name asc;"))[0];
+	return rows;
+}
+
+// Selects and orders rows by owner.name descending
+async function getHomesOwnerDesc() {
+	const rows = (await pool.query("select home.*, owner.name, sale.price from home left join owner on home.owner_ssn = owner.ssn left join sale on sale.home_id = home.home_id order by owner.name desc;"))[0];
+	return rows;
+}
+
+// Selects and orders rows by home.sqft ascending
+async function getHomesSqftAsc() {
+	const rows = (await pool.query("select home.*, owner.name, sale.price from home left join owner on home.owner_ssn = owner.ssn left join sale on sale.home_id = home.home_id order by sqft asc;"))[0];
+	return rows;
+}
+
+// Selects and orders rows by home.sqft descending
+async function getHomesSqftDesc() {
+	const rows = (await pool.query("select home.*, owner.name, sale.price from home left join owner on home.owner_ssn = owner.ssn left join sale on sale.home_id = home.home_id order by sqft desc;"))[0];
+	return rows;
+}
+// Selects and orders rows by bathroom ascending
+async function getHomesbathroomsAsc() {
+    const rows = (await pool.query("select home.*, owner.name, sale.price from home left join owner on home.owner_ssn = owner.ssn left join sale on sale.home_id = home.home_id order by bathrooms asc;"))[0];
+    return rows;
+}
+// Selects and orders rows by bathroom descending
+async function getHomesbathroomsDesc() {
+    const rows = (await pool.query("select home.*, owner.name, sale.price from home left join owner on home.owner_ssn = owner.ssn left join sale on sale.home_id = home.home_id order by bathrooms desc;"))[0];
+    return rows;
+}
+
+
+// Export to server.js
+export { getHomes, insert, update, deleteHome, getHomesOwnerAsc, getHomesOwnerDesc, getHomesSqftAsc, getHomesSqftDesc, getHomesbathroomsAsc, gethomebathroomsDesc };
---
+++ server.js
@@ -0,0 +1,92 @@
+import express from "express";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import mysql from "mysql2";
+import bodyParser from "body-parser";
+// Import functions from database.js
+// These are the functions used for querying
+import { getHomes, insert, update, deleteHome, getHomesOwnerAsc, getHomesOwnerDesc, getHomesSqftAsc, getHomesSqftDesc, getHomesbathroomsAsc, gethomebathroomsDesc } from "./database.js";
+
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __js = dirname(__filename);
+const __public = dirname(__js);
+
+app.use(express.static(__public));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+// rows is an array holding the queried data. It gets assigned to the results of a SQL query depending on what action was taken on the webpage
+// It then gets passed to the html for displaying on the webpage
+// Here, it is assigned to getHomes(), as this is the initial data displayed on the page
+let rows = await getHomes();
+
+// filter variable is used to determine whether a filter is being applied or not
+// It starts off null, meaning it is not being applied
+// It can also be assigned "asc" or "desc" depending on whether the user wants the rows to be in ascending or descending order
+let sqftFilter = null;
+let ownerFilter = null
+
+// Initial HTTP GET request upon page load
+app.get("/", async (req, res) => {
+	// Render web page and pass rows
+	res.render(__public + "/index.ejs", { rows : rows });
+});
+
+// An HTTP POST request is sent every time a button is clicked
+// req.body.btn is used to determine which filter button was clicked
+// The corresponding filter variable is used to determine whether it is ascending or descending
+app.post("/", async (req, res) => {
+
+	// + button (inserts a home with default values)
+	if (req.body.btn == "+") {
+		await insert();
+		rows = await getHomes();
+	}
+
+	// update button (updates all rows)
+	if (req.body.btn == "update") {
+		await update(req.body.id, req.body.type, req.body.sqft, req.body.floors, req.body.bedrooms, req.body.bathrooms, req.body.land_size, req.body.year);
+		rows = await getHomes();
+	}
+
+	// - button (deletes the corresponding home)
+	if (Number(req.body.btn) > 0) {
+		await deleteHome(req.body.btn);
+		rows = await getHomes();
+	}
+
+	// owner filter button
+	if (req.body.btn == "owner") {
+		if (ownerFilter == null || ownerFilter == "desc") { // Filter is now applied in ascending order
+			ownerFilter = "asc";
+			rows = await getHomesOwnerAsc();
+		} else if (ownerFilter == "asc") { // Filter is now applied in descending order
+			ownerFilter = "desc"
+			rows = await getHomesOwnerDesc();
+		}
+	}
+
+	// sqft filter button
+	if (req.body.btn == "sqft") {
+		if (sqftFilter == null || sqftFilter == "desc") { // Filter is now applied in ascending order
+			sqftFilter = "asc";
+			rows = await getHomesSqftAsc();
+		} else if (sqftFilter == "asc") { // Filter is now applied in descending order
+			sqftFilter = "desc"
+			rows = await getHomesSqftDesc();
+		}
+	}
+
+	// X button (removes filters)
+	if (req.body.btn == "x") {
+		ownerFilter = null;
+		sqftFilter = null;
+		rows = await getHomes();
+	}
+
+	// Page is reloaded with updated rows
+	res.redirect("/");
+});
+
+app.listen(3000, () => console.log("Server started on port 3000"));
